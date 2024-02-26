8017928349580a39a2be5fcbb79373ad91e2246e
Nineresh <1990andreaslindblad@gmail.com>
2024-02-26 15:17:02 +0100

    1.0

--- passwordCombination/main.py
+++ passwordCombination/main.py
@@ -1,19 +1,17 @@
-
 matrixx = []
 matrixy = []
 matrixxy = []
-listOfstring = ["agesson","ageskar","admin","DBEED5ACFB"]
-for user in range (0,len(listOfstring)):
+listOfstring = ["agesson", "ageskar", "admin", "DBEED5ACFB", "Hej", "påDig", "din luring"]
+
+for user in range(len(listOfstring)):
     targetString = listOfstring[user]
     for password in listOfstring:
         matrixx.append(targetString)
         matrixy.append(password)
 
-for index in range (0,len(matrixx)):
-    matrixxy.append([matrixx[index]],[matrixy[index]])
-
+for index in range(len(matrixx)):
+    matrixxy.append([matrixx[index], matrixy[index]])
 
 for row in matrixxy:
-    for element in row:
-        print(element)
-
+    print(row)
+    
